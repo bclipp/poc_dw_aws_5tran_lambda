073e9f4cce0e7db1476fb9b795167daef07c53c1
blipp <bclipp770@gmail.com>
2021-02-02 12:28:41 -0500

    init commit

---
+++ app/lambda_function.py
@@ -0,0 +1,8 @@
+def lambda_handler(request, context):
+    # get env rest endpoint
+    # pull data from rest api
+    # built response
+    # return response
+    # example https: // github.com / fivetran / functions / blob / master / redshift / aws_lambda / lambda_function.py
+    # docs https://fivetran.com/docs/functions
+    return reponse
