cc35534240ac59ad8a5aaa69e605f3057670246d
blipp <bclipp770@gmail.com>
2021-02-02 12:56:02 -0500

    starting the create response function

--- app/api.py
+++ app/api.py
@@ -5,7 +5,7 @@ import backoff
 @backoff.on_exception(backoff.expo,
                       (requests.exceptions.Timeout,
                        requests.exceptions.ConnectionError))
-def census_api(url: str) -> dict:
+def app_api(url: str) -> dict:
     try:
         result = requests.get(url)
     except requests.exceptions.RequestException as error:
@@ -13,3 +13,13 @@ def census_api(url: str) -> dict:
         print(error)
 
     return {"json": result.json()["results"][0], "status_code": result.status_code}
+
+
+def create_response():
+    response = dict()
+    state = ""
+    insert = ""
+    delete = ""
+    schema = ""
+    has_more = ""
+    return response
--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,14 +1,14 @@
-import requests
-import backoff
 import utils
 import api
 
 
 def lambda_handler(request, context):
     environment_variables: utils.ConfigVars = utils.get_variables()
-    api.census_api(environment_variables["URL"])
-    # built response
-    # return response
+    rest_response: dict = api.app_api(environment_variables["URL"])
+    json: dict = rest_response["json"]
+    response: dict = api.create_response(json)
+    return response
+
+
     # example https: // github.com / fivetran / functions / blob / master / redshift / aws_lambda / lambda_function.py
     # docs https://fivetran.com/docs/functions
-    return reponse
